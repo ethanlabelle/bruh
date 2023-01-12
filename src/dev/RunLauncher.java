9f5dac9c1ffc9e5b476249cb9934f65abd95d6a2
Yaro Luchko <yaro@mit.edu>
2023-01-11 22:18:11 -0500

    launcher improv

--- src/dev/RunLauncher.java
+++ src/dev/RunLauncher.java
@@ -10,11 +10,13 @@ public strictfp class RunLauncher {
      * Run a single turn for a Launcher.
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
-     
+    static boolean at_hq = false;
+
     static void runLauncher(RobotController rc) throws GameActionException {
         // Try to attack someone
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
+        MapLocation me = rc.getLocation();
         RobotInfo[] enemies = Arrays.stream(rc.senseNearbyRobots(radius, opponent)).filter(robot -> robot.type != RobotType.HEADQUARTERS).toArray(RobotInfo[]::new);
         if (enemies.length > 0) {
             MapLocation toAttack = enemies[0].location;
@@ -24,11 +26,20 @@ public strictfp class RunLauncher {
                 rc.setIndicatorString("Attacking");
                 rc.attack(toAttack);
             }
+            Direction dir = me.directionTo(toAttack);
+            if (!at_hq) {
+                if(rc.canMove(dir)) {
+                    rc.move(dir);
+                }
+            }
         }
 
-        MapLocation me = rc.getLocation();
 
         //TODO: ATTACK ENEMY HEADQUARTERS!!!!!!!!!!!!!!!!
+        if (at_hq) {
+            return;
+        }
+
         RobotInfo[] hqs = Arrays.stream(rc.senseNearbyRobots()).filter(robot -> robot.type == RobotType.HEADQUARTERS && robot.team != RobotPlayer.myTeam).toArray(RobotInfo[]::new);
         int min_dist = 7200;
         if (hqs.length >= 1) {
@@ -40,7 +51,11 @@ public strictfp class RunLauncher {
                     closest_hq = hq;
                 }
             }
-
+            int distance_to_hq = me.distanceSquaredTo(closest_hq.location);
+            if (distance_to_hq == 1 || distance_to_hq == 2){
+                at_hq = true;
+                return;
+            }
             Direction dir = me.directionTo(closest_hq.location);
             if (rc.canMove(dir)) {
                 rc.move(dir);
