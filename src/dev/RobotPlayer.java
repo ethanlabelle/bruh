29f9ffdd4016d7a705f81acbd6bcdff002bb5bae
Ethan LaBelle <ethan.labelle01@gmail.com>
2023-01-14 22:56:02 -0500

    added rng back

--- src/dev/RobotPlayer.java
+++ src/dev/RobotPlayer.java
@@ -24,8 +24,8 @@ public strictfp class RobotPlayer {
      * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
      * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
      */
-    //static final Random rng = new Random(6147);
-    static final Random rng = new Random();
+    static final Random rng = new Random(6147);
+    //static final Random rng = new Random();
 
     /** Array containing all the possible movement directions. */
     static final Direction[] directions = {
