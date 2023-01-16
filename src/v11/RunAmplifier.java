8149faf32ce16e66a218da54c1d8a5fbc958d961
Ethan LaBelle <ethan.labelle01@gmail.com>
2023-01-15 22:12:57 -0500

    v11

---
+++ src/v11/RunAmplifier.java
@@ -0,0 +1,21 @@
+package v11;
+
+import battlecode.common.*;
+
+import static v11.RobotPlayer.*;
+
+public strictfp class RunAmplifier {
+    /**
+     * Run a single turn for a Amplifier.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runAmplifier(RobotController rc) throws GameActionException {
+        // Move randomly
+        Direction dir = directions[currentDirectionInd];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        } else if (rc.getMovementCooldownTurns() == 0) {
+            currentDirectionInd = rng.nextInt(directions.length);
+        }
+    }
+}
