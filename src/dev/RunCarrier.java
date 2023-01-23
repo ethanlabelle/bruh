0771bcd2f17eb66d9de09ed7012b85f0c968b8da
Ethan LaBelle <ethan.labelle01@gmail.com>
2023-01-22 22:55:01 -0500

    bug fix

--- src/dev/RunCarrier.java
+++ src/dev/RunCarrier.java
@@ -227,6 +227,8 @@ public strictfp class RunCarrier {
         }
         for (int i = 0; i < directions.length; i++) {
             MapLocation miningLoc = wellLoc.add(directions[i]);
+			if (!miningLoc.onTheMap())
+				continue;
             if (!miningLoc.equals(me) && board[miningLoc.x][miningLoc.y] != M_STORM && rc.senseMapInfo(miningLoc).getCurrentDirection() == Direction.CENTER && rc.canMove(me.directionTo(miningLoc))) {
                 rc.move(me.directionTo(miningLoc));
                 return;
